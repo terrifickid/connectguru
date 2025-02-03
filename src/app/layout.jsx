8862324d5ed402bce514399f8b71f4094f7d9da3
msplint <tk@terrifickid.net>
2025-02-03 14:53:28 -0600

    update

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,19 +2,64 @@ import { RootLayout } from "@/components/RootLayout";
 
 import "@/styles/tailwind.css";
 
+import pageone from "@/images/pane1.webp";
+import pagetwo from "@/images/pane2.webp";
+import pagethree from "@/images/pane3.webp";
+import pagefour from "@/images/pane4.webp";
+import pagefive from "@/images/pane5.webp";
+import pagesix from "@/images/pane6.webp";
+import pageseven from "@/images/pane7.webp";
+import pageeight from "@/images/pane8.webp";
+
 export const metadata = {
   title: {
-    template: "Actai Advisors",
-    default:
-      "Actai Advisors - Accelerating innovation for startups, investors, and fund managers.",
+    template: "Connect.guru",
+    default: "Connect.guru",
   },
 };
 
 export default function Layout({ children }) {
   return (
-    <html lang="en" className="h-full bg-neutral-950 text-base antialiased">
-      <body className="flex min-h-full flex-col">
-        <RootLayout>{children}</RootLayout>
+    <html lang="en" className="">
+      <body className="">
+        <div class="bg-[#13151d]">
+          <div class="container mx-auto">
+            <img src={pageone.src} />
+          </div>
+        </div>
+        <div class="container mx-auto">
+          <img src={pagetwo.src} />
+        </div>
+        <div class="bg-[#13151d]">
+          <div class="container mx-auto">
+            <img src={pagethree.src} />
+          </div>
+        </div>
+        <div class="bg-[#13151d]">
+          <div class="container mx-auto">
+            <img src={pagefour.src} />
+          </div>
+        </div>
+        <div class="bg-[#13151d]">
+          <div class="container mx-auto">
+            <img src={pagefive.src} />
+          </div>
+        </div>
+        <div class="bg-[#13151d]">
+          <div class="container mx-auto">
+            <img src={pagesix.src} />
+          </div>
+        </div>
+        <div class="bg-[#13151d]">
+          <div class="container mx-auto">
+            <img src={pageseven.src} />
+          </div>
+        </div>
+        <div class="bg-[#13151d]">
+          <div class="container mx-auto">
+            <img src={pageeight.src} />
+          </div>
+        </div>
       </body>
     </html>
   );
