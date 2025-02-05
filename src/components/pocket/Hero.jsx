48d2632ee733e06e04003ad8a710bfdf02427362
msplint <tk@terrifickid.net>
2025-02-05 16:51:16 -0600

    update

--- src/components/pocket/Hero.jsx
+++ src/components/pocket/Hero.jsx
@@ -101,17 +101,19 @@ function PlayIcon(props) {
 
 export function Hero() {
   return (
-    <div className="overflow-hidden py-20 sm:py-32 lg:pb-32 xl:pb-36">
+    <div className="overflow-hidden py-20 sm:py-32 lg:pb-32 xl:pb-36 mt-10">
       <Container>
         <div className="lg:grid lg:grid-cols-12 lg:gap-x-8 lg:gap-y-20">
           <div className="relative z-10 mx-auto max-w-2xl lg:col-span-7 lg:max-w-none lg:pt-6 xl:col-span-6">
             <h1 className="text-4xl font-medium tracking-tight text-gray-900">
-              Invest at the perfect time.
+              Al-Powered Advisory Ecosystem for Founders, Innovators, and
+              Visionaries
             </h1>
             <p className="mt-6 text-lg text-gray-600">
-              By leveraging insights from our network of industry insiders,
-              you’ll know exactly when to buy to maximize profit, and exactly
-              when to sell to avoid painful losses.
+              Unlock your potential with{" "}
+              <b className="font-medium">Connect Guru</b>, where human expertise
+              meets cutting-edge Al to revolutionize collaboration,
+              decision-making, and impact globally.
             </p>
             <div className="mt-8 flex flex-wrap gap-x-6 gap-y-4">
               <AppStoreLink />
@@ -128,7 +130,7 @@ export function Hero() {
           <div className="relative mt-10 sm:mt-20 lg:col-span-5 lg:row-span-2 lg:mt-0 xl:col-span-6">
             <BackgroundIllustration className="absolute top-4 left-1/2 h-[1026px] w-[1026px] -translate-x-1/3 stroke-gray-300/70 [mask-image:linear-gradient(to_bottom,white_20%,transparent_75%)] sm:top-16 sm:-translate-x-1/2 lg:-top-16 lg:ml-12 xl:-top-14 xl:ml-0" />
             <div className="-mx-4 h-[448px] px-9 [mask-image:linear-gradient(to_bottom,white_60%,transparent)] sm:mx-0 lg:absolute lg:-inset-x-10 lg:-top-10 lg:-bottom-20 lg:h-auto lg:px-0 lg:pt-10 xl:-bottom-32">
-              <PhoneFrame className="mx-auto max-w-[366px]">
+              <PhoneFrame className="mx-auto max-w-[366px]" priority>
                 <AppDemo />
               </PhoneFrame>
             </div>
--- src/components/pocket/PrimaryFeatures.jsx
+++ src/components/pocket/PrimaryFeatures.jsx
@@ -559,13 +559,16 @@ export function PrimaryFeatures() {
       <Container>
         <div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-3xl">
           <h2 className="text-3xl font-medium tracking-tight text-white">
-            Every feature you need to win. Try it for yourself.
+            Empower your journey with Al-driven insights, expert guidance, and a
+            global network of innovators
           </h2>
           <p className="mt-2 text-lg text-gray-400">
-            Pocket was built for investors like you who play by their own rules
-            and aren’t going to let SEC regulations get in the way of their
-            dreams. If other investing tools are afraid to build it, Pocket has
-            it.
+            <b class="font-medium">Connect Guru</b> is a global platform
+            transforming how founders, innovators, and companies connect,
+            collaborate, and grow. By combining cutting-edge AI with human
+            expertise, we streamline decision-making, foster impactful
+            relationships, and unlock new opportunities for sustainable
+            innovation.
           </p>
         </div>
       </Container>
